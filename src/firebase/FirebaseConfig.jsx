c39bfc7678315f48f7149e33dd214a1375d3511c
itsabhibhagat <itsabhibhagat>
2025-07-06 17:20:59 +0530

    changed Db

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -6,13 +6,14 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD29w4yhl_s8Wu04I2UFwq8BiKPISWKmlk",
-  authDomain: "abhijeet-firstproject.firebaseapp.com",
-  projectId: "abhijeet-firstproject",
-  storageBucket: "abhijeet-firstproject.appspot.com",
-  messagingSenderId: "404306893019",
-  appId: "1:404306893019:web:63774d92f6b81908e954fe"
+  apiKey: "AIzaSyAptp-mAm_wxSlj0wdVH0A3MEtF4fNc9Bk",
+  authDomain: "artrangi-db.firebaseapp.com",
+  projectId: "artrangi-db",
+  storageBucket: "artrangi-db.firebasestorage.app",
+  messagingSenderId: "269629203504",
+  appId: "1:269629203504:web:c573fe8e07924e739f79f5"
 };
 
 // Initialize Firebase
